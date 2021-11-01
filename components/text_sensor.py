dd73aff9fda53ba584c032089a94db79707a8b0e
Fabry <fabry.pisani@gmail.com>
2021-11-01 16:24:57 +0100

    test files

--- components/text_sensor.py
+++ components/text_sensor.py
@@ -24,18 +24,19 @@ owm_ns = cg.esphome_ns.namespace("openweathermap")
 LANGUAGES = {"LANG_IT": "it", "LANG_DE": "de", "LANG_EN": "en"}
 
 # OpenWeatherMapClient = owm_ns.class_("openweathermapclient", cg.Component)
-OpenWeatherMapClient = owm_ns.class_("openweathermapclient", text_sensor.TextSensor, cg.PollingComponent)
+OpenWeatherMapClient = owm_ns.class_("openweathermapclient", cg.PollingComponent, text_sensor.TextSensor)
 
 CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(OpenWeatherMapClient),
         cv.Optional(CONF_API_KEY): cv.string,
-        cv.Optional(CONF_LANG): cv.ensure_list(cv.In(LANGUAGES)),
-        cv.Optional(CONF_CITY_ID): cv.positive_int
+        # cv.Optional(CONF_LANG): cv.ensure_list(cv.In(LANGUAGES)),
+        # cv.Optional(CONF_CITY_ID): cv.positive_int
     }
-).extend(cv.polling_component_schema("60s")).extend(cv.COMPONENT_SCHEMA)
+).extend(cv.polling_component_schema("60s"))
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+    await text_sensor.register_text_sensor(var, config)
+
