39cb09f3ca5c16a475b8f8590933f8e7e7d66142
Fabry <fabry.pisani@gmail.com>
2021-11-01 21:35:57 +0100

    changed lang settings

--- components/openweathermap/text_sensor.py
+++ components/openweathermap/text_sensor.py
@@ -30,7 +30,7 @@ CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(OpenWeatherMapClient),
         cv.Required(CONF_API_KEY): cv.string_strict,
-        cv.Optional(CONF_LANG, default="en"): cv.one_of(LANGUAGES),
+        cv.Optional(CONF_LANG, default="en"): cv.one_of(list(LANGUAGES), lower=True),
         # cv.Optional(CONF_CITY_ID): cv.positive_int
     }
 ).extend(cv.polling_component_schema("60s"))
